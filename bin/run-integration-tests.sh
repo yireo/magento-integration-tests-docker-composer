5e7df75173a43891ea28273f72f5ef10226e1ea8
Jisse Reitsma <jisse@yireo.com>
2024-06-12 20:26:55 +0200

    Remove composer at test runs

--- bin/run-integration-tests.sh
+++ bin/run-integration-tests.sh
@@ -7,8 +7,6 @@ cp magento-files/phpunit.xml src/dev/tests/integration/
 cp magento-files/install-config-mysql.php src/dev/tests/integration/etc/
 
 docker-compose exec -T --user www-data -w /var/www/html php-fpm bash <<EOF
-composer require $COMPOSER_PACKAGE:$COMPOSER_VERSION
-
 cd dev/tests/integration
 test -d ../../../vendor/$COMPOSER_PACKAGE/Test/Integration/ || exit
 MAGENTO_MODULE=$MODULE_NAME php -d memory_limit=-1 ../../../vendor/bin/phpunit ../../../vendor/$COMPOSER_PACKAGE/Test/Integration/
