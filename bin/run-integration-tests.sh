7a6a0278eeb9c34b114a37d12274a8ff96d3a35e
Jisse Reitsma <jisse@yireo.com>
2024-06-14 09:51:00 +0200

    Add colors

--- bin/run-integration-tests.sh
+++ bin/run-integration-tests.sh
@@ -9,6 +9,6 @@ cp magento-files/install-config-mysql.php src/dev/tests/integration/etc/
 docker-compose exec -T --user www-data -w /var/www/html php-fpm bash <<EOF
 cd dev/tests/integration
 test -d ../../../vendor/$COMPOSER_PACKAGE/Test/Integration/ || exit
-MAGENTO_MODULE=$MODULE_NAME php -d memory_limit=-1 ../../../vendor/bin/phpunit ../../../vendor/$COMPOSER_PACKAGE/Test/Integration/
+MAGENTO_MODULE=$MODULE_NAME php -d memory_limit=-1 ../../../vendor/bin/phpunit --color ../../../vendor/$COMPOSER_PACKAGE/Test/Integration/
 EOF
 
